4bacf67ea2562dfc9544b3005f575f710f9cb2be
enes1983 <119852770+enes1983@users.noreply.github.com>
2023-01-04 09:07:48 +0100

    Add files via upload

---
+++ assigment3/assignment3/test_python.py
@@ -0,0 +1,16 @@
+someNumber = 10
+
+if(someNumber > 0):
+    print("Over zerro")
+elif(someNumber < 0):
+    print("Bellow zerro")
+else:
+    print("zerro")
+    
+    
+    #testirao sam program izmjenjujuci vrijednosti someNumera i dosao do greske u liniji 3
+
+    
+    
+    
+    
